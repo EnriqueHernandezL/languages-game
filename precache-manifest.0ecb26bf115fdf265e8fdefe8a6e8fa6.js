bc944a4f07bd98f4d98c73d7d91c6b9719e17d58
EnriqueHernandezL <enrique.hl.7@hotmail.com>
2020-01-03 13:56:31 +0100

    Updates

---
+++ precache-manifest.0ecb26bf115fdf265e8fdefe8a6e8fa6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ab425c617b5f949fc1dbd9e4a8d866f",
+    "url": "/languages-game/index.html"
+  },
+  {
+    "revision": "b5e855bcc0563381dc1c",
+    "url": "/languages-game/static/css/main.9ad2a4f8.chunk.css"
+  },
+  {
+    "revision": "da1878a3d95b65139cc2",
+    "url": "/languages-game/static/js/2.8ba609bb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/languages-game/static/js/2.8ba609bb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b5e855bcc0563381dc1c",
+    "url": "/languages-game/static/js/main.a6317838.chunk.js"
+  },
+  {
+    "revision": "d599937866474333e201",
+    "url": "/languages-game/static/js/runtime-main.80616f54.js"
+  }
+]);
